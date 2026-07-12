ce119f641093cf8db1e5ebb5d2deed104c1fd3bc
ZenOrbits <zenorbits29@gmail.com>
2026-07-12 16:18:12 +0530

    ajsa

--- Backend/server.js
+++ Backend/server.js
@@ -6,21 +6,6 @@ const userRoutes = require('./routes/auth.routes');
 dotenv.config();
 const app = express();
 
-const allowedOrigins = [
-    'http://localhost:5173',
-    'https://asset-flow-sandy.vercel.app',
-];
-
-app.use(cors({
-    origin: function (origin, callback) {
-        // allow requests with no origin (like Postman, curl, mobile apps)
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
-}));
 
 app.use(express.json());
 
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,6 +7,22 @@ const userRoutes = require('./routes/auth.routes');
 dotenv.config();
 const app = express();
 
+const allowedOrigins = [
+    'http://localhost:5173',
+    'https://asset-flow-sandy.vercel.app',
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin (like Postman, curl, mobile apps)
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+}));
+
 app.use(cors());
 app.use(express.json());
 
