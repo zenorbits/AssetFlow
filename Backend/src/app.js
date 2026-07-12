fff75763fabe6c335c5fafda898eb08bf1d53c94
ZenOrbits <zenorbits29@gmail.com>
2026-07-12 16:30:56 +0530

    sas

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 const allowedOrigins = [
     'http://localhost:5173',
-    'https://asset-flow-sandy.vercel.app',
+    'https://assetflowbackedn.onrender.com',
 ];
 
 const corsOptions = {
